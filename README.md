36128b5bf1993c93e5baab6113da7cac074c00c3
Hiren <hirenbudhrani@gmail.com>
2025-02-12 12:20:32 +0100

    adds sun and star sign (zodiac) images

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ### TO-DO (in no particular order)
 [ ] i18n translation setup for future
 [x] get full moon type images in from figma
+[x] get full sun sign images from somewhere
 [ ] accuarate moon images for each day
 [ ] 3d render of moon
 [ ] favico and title
--- components/widgets/SunSign.vue
+++ components/widgets/SunSign.vue
@@ -1,9 +1,15 @@
-<!-- components/widgets/SunSign.vue -->
+// components/widgets/SunSign.vue
 <template>
     <UiBaseCard :title="props.title" :mode="props.mode" :colSpan="props.colSpan">
-        <div class="flex items-center justify-between">
-            <span class="moon-text-secondary">Sun Sign:</span>
-            <span class="moon-text-primary">{{ moonData?.moon?.zodiac?.sun_sign }}</span>
+        <div class="flex items-center gap-4">
+            <img :src="getZodiacImage(moonData?.moon?.zodiac?.sun_sign)" :alt="moonData?.moon?.zodiac?.sun_sign"
+                class="w-12 h-12 object-contain" />
+            <div class="flex flex-col flex-1">
+                <div class="flex items-center justify-between">
+                    <span class="moon-text-primary">{{ moonData?.moon?.zodiac?.sun_sign }}</span>
+                </div>
+                <p class="text-sm opacity-75 mt-1">{{ getZodiacDescription(moonData?.moon?.zodiac?.sun_sign) }}</p>
+            </div>
         </div>
     </UiBaseCard>
 </template>
@@ -11,6 +17,18 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 import { useMoonStore } from '@/stores/moon'
+import ariesIcon from '~/assets/images/zodiac/aries.svg'
+import taurusIcon from '~/assets/images/zodiac/taurus.svg'
+import geminiIcon from '~/assets/images/zodiac/gemini.svg'
+import cancerIcon from '~/assets/images/zodiac/cancer.svg'
+import leoIcon from '~/assets/images/zodiac/leo.svg'
+import virgoIcon from '~/assets/images/zodiac/virgo.svg'
+import libraIcon from '~/assets/images/zodiac/libra.svg'
+import scorpioIcon from '~/assets/images/zodiac/scorpio.svg'
+import sagittariusIcon from '~/assets/images/zodiac/sagittarius.svg'
+import capricornIcon from '~/assets/images/zodiac/capricorn.svg'
+import aquariusIcon from '~/assets/images/zodiac/aquarius.svg'
+import piscesIcon from '~/assets/images/zodiac/pisces.svg'
 
 interface Props {
     title?: string
@@ -19,9 +37,53 @@ interface Props {
 }
 
 const props = withDefaults(defineProps<Props>(), {
+    title: 'Sun Sign',
     mode: 'astrology',
     colSpan: 1
 })
 
 const { moonData } = storeToRefs(useMoonStore())
+
+// Map zodiac signs to their imported images
+const zodiacImages = {
+    'Aries': ariesIcon,
+    'Taurus': taurusIcon,
+    'Gemini': geminiIcon,
+    'Cancer': cancerIcon,
+    'Leo': leoIcon,
+    'Virgo': virgoIcon,
+    'Libra': libraIcon,
+    'Scorpio': scorpioIcon,
+    'Sagittarius': sagittariusIcon,
+    'Capricorn': capricornIcon,
+    'Aquarius': aquariusIcon,
+    'Pisces': piscesIcon,
+}
+
+// Function to get the correct zodiac image based on sign
+const getZodiacImage = (sign: string | undefined) => {
+    if (!sign) return aquariusIcon // Default fallback
+    return zodiacImages[sign as keyof typeof zodiacImages] || aquariusIcon
+}
+
+// Optional: Add descriptions for each sign
+const zodiacDescriptions = {
+    'Aries': 'The first sign of the zodiac, ruled by Mars',
+    'Taurus': 'The fixed earth sign, ruled by Venus',
+    'Gemini': 'The mutable air sign, ruled by Mercury',
+    'Cancer': 'The cardinal water sign, ruled by the Moon',
+    'Leo': 'The fixed fire sign, ruled by the Sun',
+    'Virgo': 'The mutable earth sign, ruled by Mercury',
+    'Libra': 'The cardinal air sign, ruled by Venus',
+    'Scorpio': 'The fixed water sign, ruled by Pluto',
+    'Sagittarius': 'The mutable fire sign, ruled by Jupiter',
+    'Capricorn': 'The cardinal earth sign, ruled by Saturn',
+    'Aquarius': 'The fixed air sign, ruled by Uranus',
+    'Pisces': 'The mutable water sign, ruled by Neptune'
+}
+
+const getZodiacDescription = (sign: string | undefined) => {
+    if (!sign) return ''
+    return zodiacDescriptions[sign as keyof typeof zodiacDescriptions] || ''
+}
 </script>
